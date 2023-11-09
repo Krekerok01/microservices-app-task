c47a6258c0619a752a628773df992c8d5366100e
VarvaraMamatsiuk <varvara.mamatsiuk@specific-group.com>
2023-11-09 16:37:54 +0300

    Added an entity and sql script

---
+++ blog/src/main/java/com/specificgroup/blog/entity/Post.java
@@ -0,0 +1,38 @@
+package com.specificgroup.blog.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "posts")
+@Setter
+@Getter
+@Builder
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "postsSeqGenerator")
+    @SequenceGenerator(name = "postsSeqGenerator", sequenceName = "posts_seq", allocationSize = 1)
+    @Column(name = "post_id")
+    private Long postId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @Column(name = "modification_date")
+    private LocalDateTime modificationDate;
+}
