70cb62c4080df39c5c3e62026f61130f4471ee10
Vlad-Savko <149592031+Vlad-Savko@users.noreply.github.com>
2023-11-13 09:33:10 +0300

    Feature: minimal front-end + user-service fixes (#8)
    * user-service + docker-compose additions + user-service-db
    
    * Feature: minimal front-end

---
+++ blog-frontend/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import PostsComponent from "./component/PostsComponent";
+
+function App() {
+    return (
+        <PostsComponent/>
+    );
+}
+
+export default App;
--- user/src/main/java/com/specificgroup/user/controller/UserController.java
+++ user/src/main/java/com/specificgroup/user/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/users")
+@RequestMapping("users")
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
