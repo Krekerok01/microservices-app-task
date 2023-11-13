5279476f8db13681f22a47a9fb5abecb591330f1
VarvaraMamatsiuk <varvara.mamatsiuk@specific-group.com>
2023-11-13 11:55:20 +0300

    Updated logic to find posts

--- blog/src/main/java/com/specificgroup/blog/repository/PostRepository.java
+++ blog/src/main/java/com/specificgroup/blog/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.specificgroup.blog.repository;
 
 import com.specificgroup.blog.entity.Post;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAll(Specification<Post> specification);
 }
