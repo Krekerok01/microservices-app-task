d8cea783616a7aa07aef58a931ab31501a65e829
VarvaraMamatsiuk <varvara.mamatsiuk@specific-group.com>
2023-11-21 17:11:03 +0300

    Fix: Fix cors

--- gateway/src/main/java/com/specificgroup/gateway/config/WebSecurityConfig.java
+++ gateway/src/main/java/com/specificgroup/gateway/config/WebSecurityConfig.java
@@ -6,12 +6,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 public class WebSecurityConfig {
     private final CustomAuthenticationManager authenticationManager;
     private final CustomSecurityContextRepository customSecurityContextRepository;
@@ -20,6 +32,9 @@ public class WebSecurityConfig {
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
         return http.csrf()
                 .disable()
+                .cors()
+                .configurationSource(corsConfiguration())
+                .and()
                 .authorizeExchange()
                 .pathMatchers(HttpMethod.POST, "/users/**")
                 .permitAll()
@@ -27,11 +42,24 @@ public class WebSecurityConfig {
                 .permitAll()
                 .pathMatchers(HttpMethod.GET, "/subscriptions")
                 .hasAuthority("ADMIN")
-                .pathMatchers("/posts/**", "/subscriptions/**")
+                .pathMatchers("/posts/**", "/subscriptions/**", "/users/**")
                 .authenticated()
                 .and()
                 .authenticationManager(authenticationManager)
                 .securityContextRepository(customSecurityContextRepository)
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfiguration() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+
+        corsConfig.setAllowedOrigins(List.of("http://localhost:3000"));
+        corsConfig.setAllowedMethods(Arrays.asList("*"));
+        corsConfig.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
     }
 }
