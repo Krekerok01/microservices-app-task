e4f8ff8da899c7f3d15715e580e822c0dff5af5d
VarvaraMamatsiuk <varvara.mamatsiuk@specific-group.com>
2023-11-22 10:46:19 +0300

    Fix: Fix security

--- user/src/main/java/com/specificgroup/user/config/WebConfig.java
+++ user/src/main/java/com/specificgroup/user/config/WebConfig.java
@@ -13,10 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
         return new Hibernate5Module();
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry
+//                .addMapping("/**")
+//                .allowedOrigins("http://localhost:3000");
+//    }
 }
