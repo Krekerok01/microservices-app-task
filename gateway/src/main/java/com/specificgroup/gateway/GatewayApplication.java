38dcf78f4e35439b6e4fbf8627f85679ceb71bb2
VladislavSavko <vladislav.savko@specific-group.com>
2023-12-08 16:52:30 +0300

    Fix: GatewayApplication main() refactoring

--- gateway/src/main/java/com/specificgroup/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/specificgroup/gateway/GatewayApplication.java
@@ -7,9 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
 }
