2d280b0b51209b8a90bc72011bdf2287afa6953c
VarvaraMamatsiuk <varvara.mamatsiuk@specific-group.com>
2023-12-14 10:29:02 +0300

    logger settings

--- gateway/src/main/java/com/specificgroup/gateway/config/KafkaCustomLoggerAppender.java
+++ gateway/src/main/java/com/specificgroup/gateway/config/KafkaCustomLoggerAppender.java
@@ -1,36 +1,33 @@
 package com.specificgroup.gateway.config;
 
-import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
-
+@EnableKafka
 @Component
+//@Slf4j
 public class KafkaCustomLoggerAppender extends AppenderBase<ILoggingEvent> {
 
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(KafkaCustomLoggerAppender.class);
 
     @Override
     protected void append(ILoggingEvent eventObject) {
-        System.out.println("custom logger");
-        System.out.println(kafkaTemplate != null);
-    }
-
-    @PostConstruct
-    public void init() {
-        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-        context.getLoggerList()
-                .forEach(logger -> logger
-                        .addAppender(KafkaCustomLoggerAppender.this));
-
-        setContext(context);
-        start();
+        System.out.println("Custom");
+        boolean b = kafkaTemplate == null;
+        System.out.println("Kafka == null: " + b);
+        if (eventObject.getLevel().levelStr.equals("INFO")) {
+            System.out.println(eventObject.toString());
+        } else {
+            System.out.println(">>>>>>" + eventObject.toString());
+        }
     }
 }
